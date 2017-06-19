b962a3005f06ae0256a3efb7001e0ef28ec703bf
djrloria <djrloria@gmail.com>
2017-06-18 23:04:08 -0500

    added order by at api-routes.js findAll

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 var db = require('../models');
 var processor = require('./processor.js')
 var burger = db.Burger;
+var sequelize = db.sequelize;
 
 module.exports = function(app) {
     app.get('/', function(req, res) {            
