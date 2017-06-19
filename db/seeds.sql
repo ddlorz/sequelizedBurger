f394116aea87db11c1b0212625ae8e612549369e
djrloria <djrloria@gmail.com>
2017-06-18 22:35:55 -0500

    created core functionality of the application

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-USE burgers_db;
-
-INSERT INTO burgers (burger_name) 
-VALUES ('Cheese'), ('Mushroom'), ('Jalopeno');
---
+++ routes/api-routes.js
@@ -0,0 +1,38 @@
+var db = require('../models');
+var processor = require('./processor.js')
+var burger = db.Burger;
+
+module.exports = function(app) {
+    app.get('/', function(req, res) {            
+        burger.findAll({}).then(function(result) {
+            var processed = processor(result);
+            res.render('index', {burg: processed[0], eaten_burg: processed[1]});
+        });     
+    }); 
+    app.post('/api/add', function(req, res) {  
+        var burger_name = req.body.burger_name; 
+        var customer_name = req.body.customer_name;
+        burger.create({
+            burger_name: burger_name,
+            devoured: false,
+            customer: customer_name
+        }).then(function() {
+            res.end();
+        });
+    });
+    app.post('/api/update', function(req, res) {
+        var burger_id = req.body.burger_id;  
+        var eater = req.body.eater;
+        burger.update({
+            devoured: true,
+            eater: eater
+        }, {
+            where: {
+                id: burger_id
+            }
+        }).then(function() {
+            res.end();
+        });          
+    });
+}
+
